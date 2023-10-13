4a0c2289340ea363f96e5d9285f3027ab7a2836f
Feten815 <fetenriahi22@gmail.com>
2023-10-13 08:58:30 +0100

    E-commerce
    next.js project

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import Navbar from './Navbar/Navbar'
+import Footer from './Footer'
+import SessionProvider from "./SessionProvider"
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Flowmazon',
+  description: 'We make your wallet cry',
+} 
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <SessionProvider>       
+        <Navbar/>
+        <main className='m-auto min-w-[300px] p-4 max-w-7xl '>
+        {children}
+        </main>
+        <Footer/>
+        </SessionProvider>
+      </body>
+    </html>
+  )
+}
